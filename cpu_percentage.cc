0799e2c025a6d5396fbb5aca062b42f606700828
jm876913 <jm876913@ohio.edu>
2019-02-14 11:52:37 -0500

    first commit

---
+++ cpu_percentage.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdio.h>
+#include <time.h>
+using namespace std;
+
+
+
+// int user_defined_percentage; // user selected value between 1 and 100
+// while(1){
+//  measure_cpu_time(t1);
+//  while(1){
+//  mesure_cpu_time(t2);
+// time_diff = time(t2)-time(t1);
+//  if( time_diff > TIME_DELAY_CONSTANT)
+//  break;
+//  }
+//  sleep_time = calc_effective_sleep_time(time_diff, percentage);
+//  sleep(sleep_time);
+// }
