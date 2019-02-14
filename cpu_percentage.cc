5c582e8dc2f7d49dc8f7acdb5b7010e7cdc27928
jm876913 <jm876913@ohio.edu>
2019-02-14 12:19:37 -0500

    updated

--- cpu_percentage.cc
+++ cpu_percentage.cc
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <stdio.h>
 #include <time.h>
+#include <ctime>
+#include <unistd.h>
 using namespace std;
 
 
@@ -17,3 +19,31 @@ using namespace std;
 //  sleep_time = calc_effective_sleep_time(time_diff, percentage);
 //  sleep(sleep_time);
 // }
+
+int calc_effective_sleep_time(time_diff, percentage);//e takes the desired CPU percentage and the measured time of the inner while loop, and calculates how long the process should sleep:
+
+
+int main()
+{
+    int user_defined_percentage = 0;
+    cin >> user_defined_percentage;
+    if(user_defined_percentage > 100 || user_defined_percentage < 1){
+        cout <<"enter a value between 1-100" <<endl;
+        cin >> user_defined_percentage;
+    }
+    else {
+        while(1){
+            std::clock_t c_start = std::clock();
+            //std::clock_t c_end = std::clock();//ends clock time
+            while(1){
+
+            }
+
+        }
+    }
+    return 0;
+}
+
+int calc_effective_sleep_time(float time_diff, int percentage){
+
+}
