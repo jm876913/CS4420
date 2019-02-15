b2d0bb3e23f4c0ede5c877fc4dd46637a925588e
jm876913 <jm876913@ohio.edu>
2019-02-15 17:00:08 -0500

    only runs at 5%

--- cpu_percentage.cc
+++ cpu_percentage.cc
@@ -20,7 +20,7 @@ int time_delay_const = 50000;
 //  sleep(sleep_time);
 // }
 
-int calc_effective_sleep_time(double time_diff, double percentage);//e takes the desired CPU percentage and the measured time of the inner while loop, and calculates how long the process should sleep:
+double calc_effective_sleep_time(double time_diff, double percent);//e takes the desired CPU percentage and the measured time of the inner while loop, and calculates how long the process should sleep:
 
 
 int main()
@@ -28,25 +28,26 @@ int main()
     double percent = 0;
     cout << "enter a percentage" <<endl;
     cin >> percent;
-    double time_diff = 0;
+    double time_diff;
+    double c_start, c_start2;
     if(percent > 100 || percent < 1){
         cout <<"enter a value between 1-100" <<endl;
         cin >> percent;
     }
     else {
         while(1){
-            std::clock_t c_start = std::clock();
+           c_start = std::clock();
             //std::clock_t c_end = std::clock();//ends clock time
             while(1){
-                std::clock_t c_start2 = std::clock();
-                time_diff = c_start2 - c_start;
-                //cout << time_diff << endl;
+               c_start2 = std::clock();
+                time_diff = (double)(c_start2 - c_start)/CLOCKS_PER_SEC*1000000;
+               // cout << time_diff << endl;
                 if(time_diff > time_delay_const){
                     break;
                 }
-                int sleep_time = calc_effective_sleep_time(time_diff, percent);
-                cout << sleep_time << endl;
-                usleep(sleep_time);
+                double sleep_time = calc_effective_sleep_time(time_diff, percent);
+                //cout << sleep_time << endl;
+                sleep(sleep_time);
             }
             
 
@@ -55,10 +56,10 @@ int main()
     return 0;
 }
 
-int calc_effective_sleep_time(double time_diff, double percent){
+double calc_effective_sleep_time(double time_diff, double percent){
     
     double p = percent/100;
-    double sleep = time_diff*(1/p-1);
+    double sleep = time_diff*((1/p)-1);
     return sleep;
 
 }
